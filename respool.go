2559310962837bec7ecb2fb8dfd3210f7b567945
Matthias Watermann <support@mwat.de>
2023-01-09 15:26:03 +0100

    + implement use of context; * add some more debug output; # fix `Put()` removing the first element;

--- respool.go
+++ respool.go
@@ -10,6 +10,7 @@ package respool
 //lint:file-ignore ST1005 - Allow any error text
 
 import (
+	"context"
 	"errors"
 	"io"
 	"log"
@@ -17,12 +18,20 @@ import (
 )
 
 type (
+	// `TCreateFunc` is the function type that has to be passed
+	// to the `New()` function.
+	// The function is supposed the create the type of resource
+	// the pool is supposed to handle.
+	// It should return an `io.Closer` which is able to
+	// close/free/release the resource created by this function.
+	TCreateFunc func() (io.Closer, error)
+
 	// `TResPool` manages a set of resources that can be shared
 	// safely by multiple goroutines.
 	// The resource being managed must implement the `io.Closer`
 	// interface.
 	TResPool struct {
-		factory   func() (io.Closer, error)
+		factory   TCreateFunc
 		mtx       sync.Mutex
 		resources chan io.Closer
 		closed    bool
@@ -34,37 +43,46 @@ type (
 )
 
 var (
-	// `ErrPoolClose` is returned if `Close()` is called multiple times.
-	ErrPoolClose TPoolErr = errors.New("Pool already closed.")
-
-	// `ErrPoolGetClosed` is returned when a `Get()` call returns on
-	// a closed pool.
-	ErrPoolGetClosed TPoolErr = errors.New("Pool is closed.")
-
 	// `ErrPoolCapacity` is returned when `New()` is called with
 	// an invalid capacity argument.
 	ErrPoolCapacity TPoolErr = errors.New("Capacity value too small.")
+
+	// `ErrPoolClosed` is returned when a `Get()` call returns on
+	// a closed pool, or `Close()` is called multiple times.
+	ErrPoolClosed TPoolErr = errors.New("Pool is closed.")
+
+	// `ErrPoolDone` is returned if a given context is done.
+	ErrPoolDone TPoolErr = errors.New("Context is done.")
 
 	// `ErrPoolInit` is returned if `New()` has problems initialising
 	// the first `aLen` pool items.
 	ErrPoolInit TPoolErr = errors.New("Can't init Len pool elements.")
 )
 
-// `DEBUG` activates some screen output (if set `true`).
+// `DEBUG` activates some screen output (if set `true`);
+// default: `false`.
 var DEBUG = false
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-// `Cap` returns the resource pool's capacity, in units of elements.
-func (pool *TResPool) Cap() int {
+// `Cap` returns the resource pool's capacity, i.e. the number of elements.
+func (pool *TResPool) Cap() (rCap int) {
 	// Since the pool's capacity never changes after its
 	// initialisation we don't need a mutex lock here.
-	return cap(pool.resources)
+
+	rCap = cap(pool.resources)
+	if DEBUG {
+		log.Println("Cap:", rCap)
+	}
+	return
 } // Cap()
 
 // `Close` will shutdown the pool and close all existing resources.
 func (pool *TResPool) Close() error {
-	// Sync this operation with the Put operation.
+	// We don't expect a `context` here because we have
+	// to close/free our resources in any case.
+
+	// Sync this operation with the Get/Put operation.
 	pool.mtx.Lock()
 	defer pool.mtx.Unlock()
 
@@ -76,12 +94,11 @@ func (pool *TResPool) Close() error {
 		// While we want to close the pool anyway we return
 		// an error to signal that the program's current logic
 		// causes the closing attempt multiple times.
-		return ErrPoolClose
+		return ErrPoolClosed
 	}
 
 	// Set the pool as closed.
 	pool.closed = true
-
 	if DEBUG {
 		log.Println("Close:", "Closing Pool")
 	}
@@ -94,6 +111,7 @@ func (pool *TResPool) Close() error {
 	var err error
 	for r := range pool.resources {
 		if e2 := r.Close(); nil == err {
+			//TODO: wrap the error(s)
 			err = e2
 		}
 	}
@@ -101,17 +119,30 @@ func (pool *TResPool) Close() error {
 } // Close()
 
 // `Get` retrieves a resource from the pool.
-func (pool *TResPool) Get() (io.Closer, error) {
+//
+//	`aContext` A (possibly canceled) context.
+func (pool *TResPool) Get(aContext context.Context) (io.Closer, error) {
+	// Sync this operation with Close/Put operations.
+	pool.mtx.Lock()
+	defer pool.mtx.Unlock()
+
 	select {
+	// Check whether we're already done.
+	case <-aContext.Done():
+		if DEBUG {
+			log.Println("Get:", "Context is done.")
+		}
+		return nil, ErrPoolDone
+
 	// Check for a free resource.
 	case r, ok := <-pool.resources:
 		if DEBUG {
-			log.Println("Get:", "Shared Resource")
+			log.Println("Get:", "Shared Resource -- ", ok)
 		}
-		if !ok {
-			return nil, ErrPoolGetClosed
+		if ok {
+			return r, nil
 		}
-		return r, nil
+		return nil, ErrPoolClosed
 
 	// Provide a new resource since there are none available.
 	default:
@@ -119,31 +150,43 @@ func (pool *TResPool) Get() (io.Closer, error) {
 			log.Println("Get:", "New Resource")
 		}
 		return pool.factory()
-	}
+	} // select
 } // Get()
 
 // `IsClosed` tells whether the pool is already closed.
-func (pool *TResPool) IsClosed() bool {
+func (pool *TResPool) IsClosed() (rClosed bool) {
 	// Sync this operation with Close/Put operations.
 	pool.mtx.Lock()
 	defer pool.mtx.Unlock()
 
-	return pool.closed
+	rClosed = pool.closed
+	if DEBUG {
+		log.Println("IsClosed:", rClosed)
+	}
+
+	return
 } // IsClosed()
 
 // `Len` returns the number of currently unused elements in
 // the resources pool.
-func (pool *TResPool) Len() int {
+func (pool *TResPool) Len() (rLen int) {
 	// Sync this operation with the Get/Put operations.
 	pool.mtx.Lock()
 	defer pool.mtx.Unlock()
 
-	return len(pool.resources)
+	rLen = len(pool.resources)
+	if DEBUG {
+		log.Println("Len:", rLen)
+	}
+	return
 } // Len()
 
 // `Put` places a resource into the pool.
-func (pool *TResPool) Put(aResource io.Closer) {
-	// Sync this operation with the Close operation.
+//
+//	`aContext` A (possibly canceled) context.
+//	`aResource` The resource to put back into the pool.
+func (pool *TResPool) Put(aContext context.Context, aResource io.Closer) error {
+	// Sync this operation with the Close/Get operation.
 	pool.mtx.Lock()
 	defer pool.mtx.Unlock()
 
@@ -152,51 +195,91 @@ func (pool *TResPool) Put(aResource io.Closer) {
 		if DEBUG {
 			log.Println("Put:", "Queue already closed")
 		}
-		aResource.Close()
-		return
+		return ErrPoolClosed
 	}
 
+	// Flag for closing the very first resource.
+	killedOldest := false
+
 	select {
+	// Check whether we're already done.
+	case <-aContext.Done():
+		if DEBUG {
+			log.Println("Put:", "Context is done (I).")
+		}
+		return ErrPoolDone
+
 	// Try to place the resource on the queue.
 	case pool.resources <- aResource:
 		if DEBUG {
-			log.Println("Put:", "Into Queue")
+			log.Println("Put:", "Into Queue (I)")
 		}
-		return
+		return nil
 
-	// If the queue is already at cap we close the resource.
+	// If the queue is already at capacity we close a resource.
+	// First we try to close the very first one (making room
+	// for the new resource).
 	default:
 		select {
+		// Check whether we're already done.
+		case <-aContext.Done():
+			if DEBUG {
+				log.Println("Put:", "Context is done (II).")
+			}
+			return ErrPoolDone
+
 		case res, ok := <-pool.resources:
 			// Get the first/oldest pool element.
 			if ok {
-				if err := res.Close(); (nil == err) && DEBUG {
-					log.Println("Put:", "Closed oldest")
+				err := res.Close()
+				killedOldest = true
+				if DEBUG {
+					log.Println("Put:", "Closed oldest -- ", err)
 				}
 			}
+		} // select
+	}
+
+	if killedOldest {
+		// Okay, we freed a place in the pool by removing
+		// the very first/oldest resource and now we try
+		// again to put the new resource into the pool.
+		select {
+		// Again, check whether we're already done.
+		case <-aContext.Done():
+			if DEBUG {
+				log.Println("Put:", "Context is done (III).")
+			}
+			return ErrPoolDone
 
 		case pool.resources <- aResource:
 			// This time we succeeded …
-			return
+			if DEBUG {
+				log.Println("Put:", "Into Queue (II)")
+			}
 
 		default:
-			if err := aResource.Close(); (nil == err) && DEBUG {
-				log.Println("Put:", "Closed newest")
+			err := aResource.Close()
+			if DEBUG {
+				log.Println("Put:", "Closed newest -- ", err)
 			}
-		}
-	} // select
+			return err
+		} // select
+	}
+
+	return nil
 } // Put()
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // `New` creates a pool that manages resources.
-// A pool requires a function that can allocate a new resource and the
-// initial Len and the final capacity of the pool.
+// A pool requires a function that can allocate a new resource and
+// the initial Len and the final Capacity of the pool.
 //
 //	`aFunc` A user provided function that can allocate a new resource.
 //	`aLen` The number of elements to initialise at startup.
 //	`aCap` Tha maximal number of elements in the pool.
-func New(aFunc func() (io.Closer, error), aLen, aCap int) (*TResPool, TPoolErr) {
+func New(aFunc TCreateFunc, aLen int, aCap int) (*TResPool, TPoolErr) {
 	if 0 >= aCap {
 		if DEBUG {
 			log.Println("New:", "Invalid pool capacity:", aCap)
@@ -223,10 +306,10 @@ func New(aFunc func() (io.Closer, error), aLen, aCap int) (*TResPool, TPoolErr) 
 		for i := 0; i < aLen; i++ {
 			if r, err := aFunc(); nil == err {
 				select {
-				// Try placing a new resource on the queue.
+				// Try placing a new resource in the queue.
 				case rPool.resources <- r:
 					// Success: go on with the loop.
-					break
+					continue
 
 				// If the queue is already at cap we close the resource.
 				default:
